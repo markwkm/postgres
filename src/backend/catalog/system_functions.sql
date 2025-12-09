9f1855226748282cc853f951dc7aea222d9f72cc
Mark Wong <markwkm@gmail.com>
2025-12-09 11:51:39 -0800

    Handle pg_get_triggerdef default args in system_functions.sql
    Modernize pg_get_triggerdef to use CREATE OR REPLACE FUNCTION to handle
    the optional pretty argument.

--- src/backend/catalog/system_functions.sql
+++ src/backend/catalog/system_functions.sql
@@ -699,6 +699,13 @@ LANGUAGE INTERNAL
 PARALLEL SAFE
 AS 'pg_get_expr';
 
+CREATE OR REPLACE FUNCTION
+  pg_get_triggerdef(trigger oid, pretty bool DEFAULT false)
+RETURNS TEXT
+LANGUAGE INTERNAL
+PARALLEL SAFE
+AS 'pg_get_triggerdef';
+
 --
 -- The default permissions for functions mean that anyone can execute them.
 -- A number of functions shouldn't be executable by just anyone, but rather
