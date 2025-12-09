bf51146be2e34b13c0ef52a3ca29358760ca661b
Mark Wong <markwkm@gmail.com>
2025-12-09 10:59:41 -0800

    Handle pg_get_constraintdef default args in system_functions.sql
    Modernize pg_get_constraintdef to use CREATE OR REPLACE FUNCTION to
    handle the optional pretty argument.

--- src/backend/catalog/system_functions.sql
+++ src/backend/catalog/system_functions.sql
@@ -685,6 +685,13 @@ LANGUAGE INTERNAL
 PARALLEL SAFE
 AS 'pg_get_indexdef';
 
+CREATE OR REPLACE FUNCTION
+  pg_get_constraintdef("constraint" oid, pretty bool DEFAULT false)
+RETURNS TEXT
+LANGUAGE INTERNAL
+PARALLEL SAFE
+AS 'pg_get_constraintdef';
+
 --
 -- The default permissions for functions mean that anyone can execute them.
 -- A number of functions shouldn't be executable by just anyone, but rather
