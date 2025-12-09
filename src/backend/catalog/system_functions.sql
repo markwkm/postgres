b5b88b41146604ca665f2f61e944627d8f7de0b5
Mark Wong <markwkm@gmail.com>
2025-12-09 11:17:56 -0800

    Handle pg_get_expr default args in system_functions.sql
    Modernize pg_get_expr to use CREATE OR REPLACE FUNCTION to handle the
    optional pretty argument.

--- src/backend/catalog/system_functions.sql
+++ src/backend/catalog/system_functions.sql
@@ -692,6 +692,13 @@ LANGUAGE INTERNAL
 PARALLEL SAFE
 AS 'pg_get_constraintdef';
 
+CREATE OR REPLACE FUNCTION
+  pg_get_expr(expr pg_node_tree, relation oid, pretty bool DEFAULT false)
+RETURNS TEXT
+LANGUAGE INTERNAL
+PARALLEL SAFE
+AS 'pg_get_expr';
+
 --
 -- The default permissions for functions mean that anyone can execute them.
 -- A number of functions shouldn't be executable by just anyone, but rather
